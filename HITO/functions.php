769619ce18cb6e09e68c21df09570fa89a9b1bd3
tomukobayashi <scyoutec.34@gmail.com>
2020-04-16 17:31:08 +0900

    third commit

--- HITO/functions.php
+++ HITO/functions.php
@@ -1,29 +1,67 @@
 <?php
+//出力前に特殊文字を変換する
+function html_escape($word){
+	return htmlspecialchars($word, ENT_QUOTES, 'UTF-8');
+}
 
-//データを挿入する関数
-function insert_comment($dbh,$name,$comment){
-    $date = date();
-    //データを挿入
-    $sql = "INSERT INTO borad (name,comment,created) VALUE(:name,:comment,'($date)')";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindValue(':name',$name,POD::PARAM_STR);
-    $stmt->bindValue(':comment',$comment,POD::PARAM_STR);
-    if(!$stmt->execute()){
-        return 'データの読み込みに失敗しました。';
+//POSTデータを取得する
+function get_post($key){
+    if(isset($_POST[$key])){
+        $var = trim($_POST[$key]);
+        return $var;
     }
 }
 
-//データを取得する関数
-function select_comment($dbh){
-    $date = date();
-    //データを挿入
-    $sql = "SELECT name,comment,created FROM board";
+//文字列の長さをチェックする
+function check_words($word, $length) {
+
+    if(mb_strlen($word) === 0){
+        return FALSE;
+    }elseif(mb_strlen($word) > $length){
+        return FALSE;
+    }else{
+        return TRUE;
+    }
+}
+
+//データベースに接続する
+function get_db_connect() {
+try{
+    $dsn = 'mysql:dbname=sample;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = '';
+
+    $dbh = new PDO($dsn, $user, $password);
+    }catch (PDOException $e){
+       echo($e->getMessage());
+       die();
+    }
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+}
+
+//コメントを書き込む
+function insert_comment($dbh, $name, $comment){
+
+    $date = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO board (name, comment, created) VALUE (:name, :comment, '{$date}')";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
+    if(!$stmt->execute()){
+        return 'データの書き込みに失敗しました。';
+    }
+}
+
+//全コメントデータを取得する
+function select_comments($dbh) {
+
+    $sql = "SELECT name, comment, created FROM board";
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        $data[]=$row;
+        $data[] = $row;
     }
     return $data;
 }
 
-?>
